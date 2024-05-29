0c6528cb66886f0e0b59b3869d929733bd1735d1
Sergio <sergioromero48@outlook.com>
2024-05-28 19:11:59 -0500

    added 3 DoF robot arm

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/sergio/marvin-vision/src/robot_arm_pkg
+# Build directory: /home/sergio/marvin-vision/src/robot_arm_pkg/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
